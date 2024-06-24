c151ff138226c81d83da39313b9769b7af700beb
minhaz74692 <minhaz.ipe.sust@gmail.com>
2024-06-24 09:47:33 +0600

    Initial Commit

---
+++ android/app/src/main/kotlin/com/mie/chatapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mie.chatapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/screens/homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.white, // Set status bar color
+    statusBarIconBrightness:
+        Brightness.dark, // For Android, use Brightness.light or Brightness.dark
+    statusBarBrightness:
+        Brightness.light, // For iOS, use Brightness.light or Brightness.dark
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
